6fa93f78d78346f7b589fa0386284601b68ed65a
buri34 <thiroki34@gmail.com>
2026-07-18 16:54:08 +0900

    Point prerelease test at saved v2.4 client

--- test_prerelease_update.bat
+++ test_prerelease_update.bat
@@ -2,13 +2,15 @@
 chcp 65001 >nul
 setlocal
 
-if not exist "%~dp0dist\PoENavi\PoENavi.exe" (
-    echo ERROR: dist\PoENavi\PoENavi.exe がありません。
-    echo 先にテスト用v2.4.0クライアントをビルドしてください。
+set "TEST_CLIENT=%~dp0dist\PoENaviold\PoENavi.exe"
+
+if not exist "%TEST_CLIENT%" (
+    echo ERROR: dist\PoENaviold\PoENavi.exe がありません。
+    echo テスト用v2.4.0クライアントを dist\PoENaviold に配置してください。
     pause
     exit /b 1
 )
 
 set "POENAVI_UPDATE_TEST_TAG=v2.5.0"
 echo Pre-release v2.5.0 を参照するテストモードでPoENaviを起動します。
-start "" "%~dp0dist\PoENavi\PoENavi.exe"
+start "" "%TEST_CLIENT%"
