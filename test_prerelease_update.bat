4324eabdeadb1ad868ea81b6ab3cecf42d487924
buri34 <thiroki34@gmail.com>
2026-07-18 17:09:42 +0900

    Reset prerelease test notification state

--- test_prerelease_update.bat
+++ test_prerelease_update.bat
@@ -3,6 +3,7 @@ chcp 65001 >nul
 setlocal
 
 set "TEST_CLIENT=%~dp0dist\PoENaviold\PoENavi.exe"
+set "POENAVI_USER_DATA_DIR=%~dp0.prerelease-test-user-data"
 
 if not exist "%TEST_CLIENT%" (
     echo ERROR: dist\PoENaviold\PoENavi.exe was not found.
@@ -12,5 +13,7 @@ if not exist "%TEST_CLIENT%" (
 )
 
 set "POENAVI_UPDATE_TEST_TAG=v2.5.0"
+if exist "%POENAVI_USER_DATA_DIR%" rmdir /s /q "%POENAVI_USER_DATA_DIR%"
 echo Starting PoENavi in pre-release update test mode for v2.5.0.
+echo Test user data: %POENAVI_USER_DATA_DIR%
 start "" "%TEST_CLIENT%"
